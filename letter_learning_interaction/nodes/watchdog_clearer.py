7c291ef695438ad83dc612766e325538f2194b78
CHILI Demo Corner <deanna.m.hood@gmail.com>
2014-09-08 15:44:18 +0200

    Watchdog clearer which robot should run to say it's still connected

---
+++ letter_learning_interaction/nodes/watchdog_clearer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+'''Publish watchdog clears on the /watchdog_clear/device_name topic every
+time_between_clears seconds.
+'''
+#@TODO make work with msec precision
+#@TODO possibly better to just use rospy.sleep instead of the watchdog clearer class
+
+import rospy
+from watchdog.watchdog import WatchdogClearer
+
+if __name__ == "__main__":
+    #parse arguments
+    import argparse
+    parser = argparse.ArgumentParser(description='Publish watchdog clears \
+    on the /watchdog_clear/device_name topic.');
+    parser.add_argument('device_name', action="store", type=str,
+                    help='name of device which the watchdog is monitoring');
+    parser.add_argument('time_between_clears', action="store", type=float,
+                    help='name of device which the watchdog is monitoring');
+    args = parser.parse_args();
+    
+    rospy.init_node(args.device_name + '_watchdog_clearer');
+
+    wdc=WatchdogClearer('watchdog_clear/'+args.device_name, args.time_between_clears);
+    print('Starting new watchdog_clearer for '+args.device_name);
+
+
+    while not rospy.is_shutdown():
+        pass
+    
+    wdc.stop();
