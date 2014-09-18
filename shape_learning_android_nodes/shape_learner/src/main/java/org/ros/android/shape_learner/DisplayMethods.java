1edf7aca374f9556f34b505c8ba0e9f2077df9a0
Deanna Hood <deanna.m.hood@gmail.com>
2014-09-18 23:02:07 +0200

    Absorb AnimationDrawableWithEndCallback into DisplayMethods

--- shape_learning_android_nodes/shape_learner/src/main/java/org/ros/android/shape_learner/DisplayMethods.java
+++ shape_learning_android_nodes/shape_learner/src/main/java/org/ros/android/shape_learner/DisplayMethods.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.PathShape;
 import android.util.Log;
+import android.os.Handler;
 
 import org.ros.android.MessageCallable;
 import org.ros.message.Duration;
@@ -194,6 +195,64 @@ public class DisplayMethods {
             return animationDrawableWithEndCallback;
         }
 
+    }
+
+
+    /**
+     * class which allows for a callback when the animation is finished.
+     * Useful for publishing a message when a shape has finished being displayed.
+     * Taken from http://stackoverflow.com/a/6641321/3441246
+     */
+    public abstract class AnimationDrawableWithEndCallback extends AnimationDrawable {
+
+        /** Handles the animation callback. */
+        Handler mAnimationHandler;
+
+        public AnimationDrawableWithEndCallback(AnimationDrawable aniDrawable) {
+    /* Add each frame to our animation drawable */
+            for (int i = 0; i < aniDrawable.getNumberOfFrames(); i++) {
+                this.addFrame(aniDrawable.getFrame(i), aniDrawable.getDuration(i));
+            }
+        }
+
+        @Override
+        public void start() {
+            super.start();
+    /*
+     * Call super.start() to call the base class start animation method.
+     * Then add a handler to call onAnimationFinish() when the total
+     * duration for the animation has passed
+     */
+            mAnimationHandler = new Handler();
+            mAnimationHandler.postDelayed(new Runnable() {
+
+                public void run() {
+                    onAnimationFinish();
+                }
+            }, getTotalDuration());
+
+        }
+
+        /**
+         * Gets the total duration of all frames.
+         *
+         * @return The total duration.
+         */
+        public int getTotalDuration() {
+
+            int iDuration = 0;
+
+            for (int i = 0; i < this.getNumberOfFrames(); i++) {
+                iDuration += this.getDuration(i);
+            }
+
+            return iDuration;
+        }
+
+        /**
+         * Called when the animation finishes.
+         */
+        abstract void onAnimationFinish();
     }
 
 }
